76bbef376a61b78a77429dec3554a3f41af33bfa
tekito <tekito@kk.kk>
2018-11-09 13:01:03 +0900

    commit

--- hisui/PieceBishop.cpp
+++ hisui/PieceBishop.cpp
@@ -39,6 +39,9 @@ VECTOR2 PieceBishop::GetPos()
 void PieceBishop::Draw()
 {
 	DrawPos = (pos * PIECE_SIZE) + offset;
+	DrawBox(DrawPos.x + 7, DrawPos.y + 23, DrawPos.x + 57, DrawPos.y + 57, 0, true);
+	DrawTriangle(DrawPos.x + 7, DrawPos.y + 23, DrawPos.x + 57, DrawPos.y + 23, DrawPos.x + 33, DrawPos.y + 7, 0, true);
+
 	DrawBox(DrawPos.x + 8, DrawPos.y + 24, DrawPos.x + 56, DrawPos.y + 56, 0xcd853f, true);
 	DrawTriangle(DrawPos.x + 8, DrawPos.y + 24, DrawPos.x + 56, DrawPos.y + 24, DrawPos.x + 32, DrawPos.y + 8, 0xcd853f, true);
 	DrawString(DrawPos.x + 25, DrawPos.y + 28, "Šp", 0, true);
--- hisui/PieceRook.cpp
+++ hisui/PieceRook.cpp
@@ -40,6 +40,9 @@ std::vector<VECTOR2> PieceRook::isMove()
 void PieceRook::Draw()
 {
 	DrawPos = (pos * PIECE_SIZE) + offset;
+	DrawBox(DrawPos.x + 7, DrawPos.y + 23, DrawPos.x + 57, DrawPos.y + 57, 0, true);
+	DrawTriangle(DrawPos.x + 7, DrawPos.y + 23, DrawPos.x + 57, DrawPos.y + 23, DrawPos.x + 33, DrawPos.y + 7, 0, true);
+
 	DrawBox(DrawPos.x + 8, DrawPos.y + 24, DrawPos.x + 56, DrawPos.y + 56, 0xcd853f, true);
 	DrawTriangle(DrawPos.x + 8, DrawPos.y + 24, DrawPos.x + 56, DrawPos.y + 24, DrawPos.x + 32, DrawPos.y + 8, 0xcd853f, true);
 	DrawString(DrawPos.x + 25, DrawPos.y + 20, "”ò\nŽÔ", 0, true);
